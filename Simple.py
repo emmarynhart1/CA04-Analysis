21d45a306ade19ce0058ee8af6cd4f3d3cf6ef27
Emma <emmanowrries@gmail.com>
2017-05-07 15:05:49 +0100

    Refactored Dictionary Code.CSV file created but is blank

--- Simple.py
+++ Simple.py
@@ -1,5 +1,4 @@
 def read_file(changes_file):
-    # use strip to strip out spaces and trim the line.
     data = [line.strip() for line in open(changes_file, 'r')]
     return data
 
@@ -10,17 +9,14 @@ def get_commits(data):
     index = 0
     while index < len(data):
         try:
-            # parse each of the commits and put them into a list of commits
             details = data[index + 1].split('|')
-            # the author with spaces at end removed.
             commit = {'revision': details[0].strip(),
                 'author': details[1].strip(),
                 'date': details[2].strip(),
                 'number_of_lines': details[3].strip().split(' ')[1]
             }
-            # add details to the list of commits.
             commits.append(commit)
-            index = data.index(sep, index + 1)
+            index = data.index(sep)
         except IndexError:
             break
     return commits
@@ -46,17 +42,11 @@ def get_dates(commits):
             dates[date] = dates[date] + 1
 
 if __name__ == '__main__':
-    # open the file - and read all of the lines.
     changes_file = 'changes_python.log'
     data = read_file(changes_file)
     commits = get_commits(data)
 
-    # print the number of lines read
-    print(len(data))
-    #print(commits)
-    print(commits[0])
-    print(commits[1]['author'])
-    print(len(commits))
+
     
     output = open("DataAnalysis.csv", 'w')
     index = 0
@@ -69,22 +59,25 @@ if __name__ == '__main__':
         output.write(';')
         output.write(commits[index]['number_of_lines'])
         output.write(';')
-        #output.write(str(commits[index]['comment'])
-        output.write(';')
-        #output.write(str(commits[index]['changes')
         output.write('\n')
         index = index +1
     output.close()
         
            
-#keys = commits [0].keys()
-#with open('test.csv', 'wb') as output_file:
-    #dict_writer = csv.DictWriter(output_file, keys)
-    #dict_writer.writeheader()
-    #dict_writer.writerows(commits)
+keys = commits [0].keys()
+dict = open('DataAnalysis.csv', 'wb') 
+dict_writer = csv.DictWriter(dict,my_dict.keys())
+dict_writer.writeheader()
+dict_writer.writerows(commits)
+dict.close()
     
-    index = 0
-    for commit in commits:
-        print (commits[index]['comment'])
-        index = index +1
+    #index = 0
+        #for commit in commits:
+        #print (commits[index]['comment'])
+        #index = index +1
+        
+print(len(data))
+print(commits[0])
+print(commits[1]['author'])
+print(len(commits))
     
